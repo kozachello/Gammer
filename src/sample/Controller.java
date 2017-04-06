e9a480c8070b63d1124ee6121c1884c957084363
kozachello <atkozak2011@gmail.com>
2017-04-06 15:20:46 +0200

    dsfsd

---
+++ src/sample/Controller.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ * Created by Козак on 06.04.2017.
+ */
+
+public class Controller {
+
+    @FXML
+    private void yourChoice() {
+
+    }
+
+    @FXML
+    private void dropBall(ActionEvent event) {
+
+    }
+
+    private boolean hasBallBeenPlaced() {
+        return true;
+    }
+
+    private String colorOfTheBall() {
+        return "yeppa";
+    }
+
+}
---
+++ src/sample/Model.java
@@ -0,0 +1,9 @@
+package sample;
+
+/**
+ * Created by Козак on 06.04.2017.
+ */
+
+public class Model {
+
+}
